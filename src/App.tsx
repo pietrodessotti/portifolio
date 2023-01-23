9db89a1b47eb1af41a36e1323145da4b0738068d
Pietro Dessotti <73199600+pietrodessotti@users.noreply.github.com>
2023-01-23 18:42:18 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { AppRoutes } from "./Routes"
+
+/**
+ * @export
+ * @component
+ * @name App
+ * 
+ * @description
+ * Responsável pelo componente de App
+ * da aplicação.
+ */
+function App() {
+  return (
+    <AppRoutes />
+  )
+}
+
+export default App
